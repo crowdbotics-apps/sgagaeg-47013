5a3a054b0d6d15fadc0e6f2275d78455ff315def
Crowdbotics <team@crowdbotics.com>
2024-03-13 19:33:38 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sgagaeg-47013",
+  url: "https://sgagaeg-47013.botics.co",
+  api: "https://sgagaeg-47013.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
